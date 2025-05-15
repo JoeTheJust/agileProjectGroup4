242afa4ff79192893cf0e19996313d597711b5d0
DparksCarthage <dparks1@carthage.edu>
2025-05-15 13:10:01 -0500

    updated moved the queries to interface

--- code/DBInterface.cpp
+++ code/DBInterface.cpp
@@ -110,3 +110,112 @@ void DBInterface::CreateAttendanceTable() {
         cout << "Error in creating Attedance Join Table" << endl;
     }
 }
+
+
+vector<array<string,3>> DBInterface ::getdateattendenceAlph(string date, string course){
+ const char* sql = "SELECT s.firstName, s.lastName, a.status "
+      "FROM Attendance a "
+      " JOIN Students s ON s.id = a.studentID "
+      " JOIN Courses  c ON c.id = a.courseID "
+      "WHERE c.courseName = ? AND a.date = ? "
+      "ORDER BY s.lastName, s.firstName;";
+    sqlite3_stmt* stmt = nullptr;
+    vector<array<string,3>> out;
+    if (sqlite3_prepare_v2(db, sql, -1, &stmt, nullptr) == SQLITE_OK) {
+        if (sqlite3_step(stmt) == SQLITE_ROW) {
+           while (sqlite3_step(stmt) == SQLITE_ROW) {
+        array<string,3> row;
+        row[0] = (char*)sqlite3_column_text(stmt, 0);
+        row[1] = (char*)sqlite3_column_text(stmt, 1);
+        row[2] = (char*)sqlite3_column_text(stmt, 2);
+        out.push_back(row);
+      }
+        }
+    } else {
+        cout << "SQL prepare error: " << sqlite3_errmsg(db) << endl;
+    }
+
+    sqlite3_finalize(stmt);
+}
+vector<array<string,3>> DBInterface ::getdateattendenceFreqAbsent(string date, string course){
+    const char* sql = "SELECT s.firstName, s.lastName, a.status "
+      "FROM Attendance a "
+      " JOIN Students s ON s.id = a.studentID "
+      " JOIN Courses  c ON c.id = a.courseID "
+      "WHERE c.courseName = ? AND a.date = ? "
+      "  AND a.status IN('AbsentExcused','AbsentUnexcused') "
+      "ORDER BY s.lastName, s.firstName;";
+    sqlite3_stmt* stmt = nullptr;
+    vector<array<string,3>> out;
+    if (sqlite3_prepare_v2(db, sql, -1, &stmt, nullptr) == SQLITE_OK) {
+        if (sqlite3_step(stmt) == SQLITE_ROW) {
+           while (sqlite3_step(stmt) == SQLITE_ROW) {
+        array<string,3> row;
+        row[0] = (char*)sqlite3_column_text(stmt, 0);
+        row[1] = (char*)sqlite3_column_text(stmt, 1);
+        row[2] = (char*)sqlite3_column_text(stmt, 2);
+        out.push_back(row);
+      }
+        }
+    } else {
+        cout << "SQL prepare error: " << sqlite3_errmsg(db) << endl;
+    }
+
+    sqlite3_finalize(stmt);
+}
+vector<array<string,3>> DBInterface ::getdateattendenceAbsent(string date, string course){
+    const char* sql = "SELECT s.firstName, s.lastName, a.status "
+      "FROM Attendance a "
+      " JOIN Students s ON s.id = a.studentID "
+      " JOIN Courses  c ON c.id = a.courseID "
+      "WHERE c.courseName = ? AND a.date = ? "
+      "ORDER BY ("
+        "SELECT COUNT(*) FROM Attendance x "
+        " WHERE x.courseID=a.courseID "
+        "   AND x.studentID=a.studentID "
+        "   AND x.status IN('AbsentExcused','AbsentUnexcused')"
+      ") DESC, s.lastName, s.firstName;";
+    sqlite3_stmt* stmt = nullptr;
+    vector<array<string,3>> out;
+    if (sqlite3_prepare_v2(db, sql, -1, &stmt, nullptr) == SQLITE_OK) {
+        if (sqlite3_step(stmt) == SQLITE_ROW) {
+           while (sqlite3_step(stmt) == SQLITE_ROW) {
+        array<string,3> row;
+        row[0] = (char*)sqlite3_column_text(stmt, 0);
+        row[1] = (char*)sqlite3_column_text(stmt, 1);
+        row[2] = (char*)sqlite3_column_text(stmt, 2);
+        out.push_back(row);
+      }
+        }
+    } else {
+        cout << "SQL prepare error: " << sqlite3_errmsg(db) << endl;
+    }
+
+    sqlite3_finalize(stmt);
+}
+vector<array<string,2>> DBInterface::CommonAbsentscourse(string course){
+    const char* sql = "SELECT s.firstName||' '||s.lastName AS fullName, COUNT(*) AS absCount "
+      "FROM Attendance a "
+      " JOIN Students s ON s.id = a.studentID "
+      " JOIN Courses  c ON c.id = a.courseID "
+      "WHERE c.courseName = ? "
+      "  AND a.status IN('AbsentExcused','AbsentUnexcused') "
+      "GROUP BY a.studentID "
+      "ORDER BY absCount DESC;";
+    sqlite3_stmt* stmt = nullptr;
+    vector<array<string,2>> out;
+    if (sqlite3_prepare_v2(db, sql, -1, &stmt, nullptr) == SQLITE_OK) {
+        if (sqlite3_step(stmt) == SQLITE_ROW) {
+           while (sqlite3_step(stmt) == SQLITE_ROW) {
+        array<string,2> row;
+        row[0] = (char*)sqlite3_column_text(stmt, 0);
+        row[1] = (char*)sqlite3_column_text(stmt, 1);
+        out.push_back(row);
+      }
+        }
+    } else {
+        cout << "SQL prepare error: " << sqlite3_errmsg(db) << endl;
+    }
+
+    sqlite3_finalize(stmt);
+}
